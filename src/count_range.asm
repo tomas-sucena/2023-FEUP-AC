a68e938b01cb27f3ea9920c01333d477ddaa3da2
tomas-sucena <tomas21sucena@gmail.com>
2023-06-13 12:17:31 +0100

    add: implemented count_range()

---
+++ src/count_range.asm
@@ -0,0 +1,21 @@
+.text
+.global count_range
+
+count_range:
+mv t0, zero
+
+.L1:
+beqz a1, .end
+addi a1, a1, -1
+flw ft0, 0(a0)
+addi a0, a0, 4
+fge.s t1, ft0, fa0
+beqz t1, .L1
+fle.s t1, ft0, fa1
+beqz t1, .L1
+addi t0, t0, 1
+beqz zero, .L1
+
+.end:
+mv a0, t0
+ret
