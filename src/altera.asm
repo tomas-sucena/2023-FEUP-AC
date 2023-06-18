8c42fd3bea171b2082e4a670dbe15ff76f32ebc9
tomas-sucena <tomas21sucena@gmail.com>
2023-06-18 15:07:25 +0100

    refactor: redo altera.asm

--- src/altera.asm
+++ src/altera.asm
@@ -3,21 +3,22 @@
 
 altera:
 addi a0, a0, -2
+blez a0, .end
+li t4, 3
 lbu t0, 0(a1) # prev
 addi a1, a1, 1
-li t3, 3
 
 .L1:
-beqz a0, .end
-lbu t1, 0(a1) # curr
-lbu t2, 1(a1) # next
-addi t4, t0, t1
-addi t4, t4, t2
-div t4, t4, t3
-sb t4, 0(al)
-mv t0, t1
+blez a0, .end
 addi a0, a0, -1
+lbu t1, 0(a1) # current
+lbu t2, 1(a1) # next
+add t3, t0, t1
+add t3, t3, t2
+div t3, t3, t4
+sb t3, 0(a1)
 addi a1, a1, 1
+mv t0, t1
 beqz zero, .L1
 
 .end:
